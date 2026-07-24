36874520c761792787720ea41a21a3eb5c993df4
TimotheeRen <timothee.ren@proton.me>
2026-07-24 10:56:06 +0200

    feat: add animations in hero and navbar

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,19 +1,28 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { IconMail } from '@tabler/icons-react';
 import { Ripple } from "./ui/ripple";
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
     <div>
       <Ripple mainCircleOpacity={0.2} className="-z-10" />
-      <h1 className="text-7xl font-extrabold mb-6">DevOps learner, <span className="text-primary">building</span> and <span className="text-primary">maintaning</span> systems</h1>
-      <p className="text-lg mb-8 text-muted-foreground">From CI/CD pipelines to containerized production environments and monitoring</p>
+      <motion.h1 animate={{ y: -20, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100 }} className="text-7xl font-extrabold mb-6 opacity-0">
+        DevOps learner, <span className="text-primary">building</span> and <span className="text-primary">maintaning</span> systems
+      </motion.h1>
+      <motion.p animate={{ y: -20, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100, delay: 0.1 }} className="text-lg mb-8 text-muted-foreground opacity-0">From CI/CD pipelines to containerized production environments and monitoring</motion.p>
       <div className="flex gap-3 justify-center">
-        <Button className="shadow-sm hover:scale-105 p-6 flex gap-3">
-          <p>Get in touch</p>
-          <IconMail />
-        </Button>
-        <Button variant="outline" className="hover:bg-accent-foreground shadow-sm p-6">Who I am</Button>
+        <motion.div animate={{ y: -20, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100, delay: 0.2 }} initial={{ opacity: 0 }}>
+          <Button className="shadow-sm hover:scale-105 p-6 flex gap-3">
+            <p>Get in touch</p>
+            <IconMail />
+          </Button>
+        </motion.div>
+        <motion.div animate={{ y: -20, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100, delay: 0.3 }} initial={{ opacity: 0 }}>
+          <Button variant="outline" className="hover:bg-accent-foreground shadow-sm p-6">Who I am</Button>
+        </motion.div>
       </div>
     </div>
   )
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,24 @@
+"use client"
+
+import { motion } from "motion/react";
 import { AnimatedThemeToggler } from "./ui/animated-theme-toggler";
 
 export default function Navbar() {
   return (
     <header className="top-0 left-0 right-0 flex items-center justify-between w-full pt-5 px-6">
-      <span className="font-semibold">Timothee Ren</span>
+      <motion.span initial={{ scale: 0.9, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100, delay: 0.7 }} className="font-semibold">Timothee Ren</motion.span>
 
-      <nav className="fixed left-1/2 -translate-x-1/2 flex gap-8 bg-background/20 rounded-full border px-6 py-2 backdrop-blur-md">
+      <motion.nav initial={{ scale: 0.9, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100, delay: 0.5 }} className="fixed left-1/2 -translate-x-1/2 flex gap-8 bg-background/20 rounded-full border px-6 py-2 backdrop-blur-md">
         <button className="cursor-pointer">Presentation</button>
         <button className="cursor-pointer">Skills</button>
         <button className="cursor-pointer">Projects</button>
         <button className="cursor-pointer">Education</button>
         <button className="cursor-pointer">Contact</button>
-      </nav>
+      </motion.nav>
 
-      <AnimatedThemeToggler className="cursor-pointer" />
+      <motion.div initial={{ scale: 0.9, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ duration: 0.75, type: "spring", stiffness: 100, delay: 0.7 }}>
+        <AnimatedThemeToggler className="cursor-pointer" />
+      </motion.div>
     </header>
   );
 }
